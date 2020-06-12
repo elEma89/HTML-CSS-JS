379d5c69b9b5c3c744c27a5a98d7a4d65cb947f9
elEma89 <61033552+elEma89@users.noreply.github.com>
2020-06-11 23:48:47 -0400

    Add files via upload

---
+++ Web Jumancri/main.js
@@ -0,0 +1,27 @@
+$(document).ready(main);
+
+var contador=1;
+
+function main() {
+    $('.menu_bar').click(function () {
+        if (contador==1) {
+            $('nav').animate( {
+                left:'0'
+            });
+            contador=0;
+            
+        }else {
+            contador=1;
+            $('nav').animate( {
+                left:'-100%'
+            });
+        }
+
+    });
+
+    // mostramos y ocultamos submenus
+
+    $('.submenu').click(function () {
+        $(this).children('.children').slideToggle();
+    });
+}
